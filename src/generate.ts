e683ed31898a3c85f660484c7651158e8052c042
Utsob Roy <roy@utsob.me>
2023-03-01 10:34:13 +0600

    timestamped date

--- src/generate.ts
+++ src/generate.ts
@@ -8,15 +8,18 @@ const dimension: [number, number] = [3024, 1964]
 const { arcs } = getPoemCalculations(poemData, reverse, dimension);
 
 const svg = generateSVG(arcs, dimension);
+let today = new Date().toISOString().split('T')[0]
 
 svg2img(svg, function(error: any, buffer: any) {
     //returns a Buffer
     fs.writeFileSync('dist/daily.png', buffer);
+    fs.writeFileSync(`dist/${today}-daily.png`, buffer);
 });
 
 const svg_dark = generateSVG(arcs, dimension, true);
 
 svg2img(svg_dark, function(error: any, buffer: any) {
     //returns a Buffer
-    fs.writeFileSync('dist/daily_dark.png', buffer);
+    fs.writeFileSync('dist/daily-dark.png', buffer);
+    fs.writeFileSync(`dist/${today}-daily-dark.png`, buffer);
 });
